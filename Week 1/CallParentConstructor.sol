32065a1ce91cebd791d769df351f3c1204aaae35
RittikPradhan <37213303+RittikPradhan@users.noreply.github.com>
2022-03-20 12:44:36 +0530

    Add files via upload

---
+++ Week 1/CallParentConstructor.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: None
+pragma solidity ^0.8.13;
+contract Parent {
+    constructor(uint a) public {
+        //do some stuff
+    }
+}
+
+contract MyContract is Parent {
+    constructor(uint _a) Parent(_a) public {
+        //Can be empty
+        //This pattern is used when we define ERC20 tokens
+    }
+}
